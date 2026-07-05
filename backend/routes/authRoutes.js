e44309ca06cc9d2f8b35673a3bae42bbd570c06a
anjalii0701 <anjalisharma288863@gmail.com>
2026-07-06 01:31:36 +0530

    fix auth import

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import {register ,login ,getMe} from "../controllers/authcontroller.js";
+import {register ,login ,getMe} from "../controllers/authController.js";
 import {protect} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
