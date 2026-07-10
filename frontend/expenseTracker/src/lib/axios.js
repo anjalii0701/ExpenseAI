12aa6d1ecddf2959eef2f1e982c19f15453cb925
anjalii0701 <anjalisharma288863@gmail.com>
2026-07-10 18:50:04 +0530

    debug api

--- frontend/expenseTracker/src/lib/axios.js
+++ frontend/expenseTracker/src/lib/axios.js
@@ -1,33 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+  import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 
-const api = axios.create({ baseURL: API_URL });
+console.log("==========");
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+console.log("API_URL =", API_URL);
+console.log("==========");
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+const api = axios.create({
+  baseURL: API_URL,
 });
-
-api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (
-      err.response?.status === 401 &&
-      window.location.pathname !== '/login'
-    ) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
-    }
-
-    return Promise.reject(err);
-  }
-);
 
 export default api;
